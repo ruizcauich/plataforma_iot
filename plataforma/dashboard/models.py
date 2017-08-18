2f13b16ab7b9d418bab03926ee812ffe84eef0b6
ruizcauich <augusto.ruizcauich@gmail.com>
2017-08-18 18:38:09 -0500

    Creacion de modelos y migración de los mismos

--- plataforma/dashboard/models.py
+++ plataforma/dashboard/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Proyecto(models.Model):
+    nombre_de_proyecto = models.CharField( max_length = 80)
+    descripcion = models.TextField()
+
+
+class Dispositivo(models.Model):
+    nombre_de_dispositivo = models.CharField(max_length = 80)
+    descripcion = models.TextField()
+    tipo = models.CharField(max_length=40)
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+    esta_habilitado = models.BooleanField(default = True)
+    fecha_creacion = models.DateTimeField( auto_now_add=True )
+    # apikey no está implemetado por el momento
+    # apikey
+
+
+class Sensor(models.Model):
+    nombre_de_sensor = models.CharField(max_length=40)
+    tipo = models.CharField(max_length = 40)
+    fecha_creacion = models.DateTimeField( auto_now_add=True )
+    esta_habilitado = models.BooleanField(default = True)
+
+class Campo( models.Model):
+    nombre_de_campo = models.CharField(max_length=30)
+    tipo_de_valor = models.CharField(max_length=40)
+
+class Valor(models.Model):
+    valor = models.CharField(max_length=100)
+    fecha_hora_lectura = models.DateTimeField(auto_now_add=True)
