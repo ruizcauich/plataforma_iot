cbc6f36a561e3032e7903bfb18983ed4097f4d09
Augusto Neftalí Ruiz Cauich <mgr.ruiz.56@gmail.com>
2018-05-08 08:25:32 -0500

    Mejoras en reportes/graficar

--- plataforma/reportes/urls.py
+++ plataforma/reportes/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^$', views.index ,name='index'),
     url(r'^graficar/([0-9]+)$', views.graficar ,name='graficar'),
     url(r'^obtener-datos/([0-9]+)$', views.obtenerDatos ,name='obtener-datos'),
+    url(r'^obtener-realtime/([0-9]+)$', views.obtenerUltimasLecturas ,name='obtener-realtime'),
     url(r'^estadisticas/$', views.index ,name='estadisticas'),
     
 ]
